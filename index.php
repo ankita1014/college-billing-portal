4e44e5f87d6d5ca48ef141aa3994f3beaa2b1989
ankita1014 <167628988+ankita1014@users.noreply.github.com>
2024-04-20 19:50:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+   <title> Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel ="stylesheet" href="istyle.css">
+</head>
+
+<body ><font size="-1">
+
+<h1 style=" background: -webkit-linear-gradient(rgb(1, 30, 241), rgb(10, 4, 4));
+   color: white; font-size: 8px; word-spacing: 0.5em;"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DTE Code: 6010&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; MSBTE Code: 0011<br>  &nbsp;<h1>
+
+    <div class="wrapper">
+        <img style="
+    max-width: 140px;float: left; padding: 0px; margin: 900;"
+src="logo2.png" alt="">
+        <div class="text-box">
+<h2 align="center" >
+<BR>
+   <b> GOVERNMENT POLYTECHNIC KOLHAPUR</b><br>
+	<b>शासकीय तंत्रनिकेतन कोल्हापूर</b>
+	
+	<br>
+	
+	
+</h2>
+<h2  align="center"  style="font-size:13px"><b>(First Autonomous Institute Of Government Of Maharashtra)<b>
+	</h2>
+<h1 align ="center" style=" color: #bf360c; font-size: 26px; font-family:Cursive">
+<b> <i>GPKP Billing Portal<i><b>
+</h1>
+
+        </div>
+    </div>
+<h1 style="background: -webkit-linear-gradient(rgb(1, 30, 241), rgb(10, 4, 4)); font-size: 21px; "><br></h1>
+<!--
+<h2 align="center" style=" color:#b71c1c; font-size: 85px;">
+
+   <b> GPKP Billing Portal</b><br>
+	
+	
+</h2>-->
+<div class="navbar"><b>
+  <a href="http://gpkolhapur.ac.in/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home&nbsp;&nbsp;</a>
+  <a href="#news">&nbsp;&nbsp;LAC&nbsp;&nbsp;</a>
+  <a href="#news">&nbsp;&nbsp;TH/Supervision Billing&nbsp;&nbsp;</a>
+  <a href="oralpr.html ">&nbsp;&nbsp;PR/OR Billing&nbsp;&nbsp;</a>
+  </b>
+  </div>
+</div>
+
+
+
+
+
+<!--
+<h3 >
+<ul>
+  <li><a href="http://gpkolhapur.ac.in/">&nbsp;&nbsp;Home&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+  <li><a href="">&nbsp;&nbsp;LAC&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+  <li><a href="http://localhost/registrations/demo.php">&nbsp;&nbsp;TH/Supervision Billing&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+  <li><a href="oralpr.html">&nbsp;&nbsp;PR/OR Billing&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+</ul>
+</h3>-->
+<img style="padding: 0px; margin:70;   background-size: 100%;
+" src="gpkp.jpeg"  width="100%" height="50%">
+
+<br>
+
+</style>
+</head>       
+</body>
+</html>
